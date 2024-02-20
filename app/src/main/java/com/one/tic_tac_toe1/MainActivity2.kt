c563b36c17dc7cb0c75c5bd8f748daa7114379d9
SOMNATH <somnathnikam2076@gmail.com>
2024-02-20 22:32:04 +0530

    Some Modification

--- app/src/main/java/com/one/tic_tac_toe1/MainActivity2.kt
+++ app/src/main/java/com/one/tic_tac_toe1/MainActivity2.kt
@@ -2,10 +2,14 @@ package com.one.tic_tac_toe1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val id = findViewById<TextView>(R.id.tv)
+        
     }
 }
