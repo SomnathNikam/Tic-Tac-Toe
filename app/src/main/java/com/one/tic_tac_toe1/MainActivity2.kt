5c11d91bf94df5234fec8204b4b56073a0f7ab89
SOMNATH <somnathnikam2076@gmail.com>
2024-02-20 22:37:23 +0530

    Added Splash Screen

--- app/src/main/java/com/one/tic_tac_toe1/MainActivity2.kt
+++ app/src/main/java/com/one/tic_tac_toe1/MainActivity2.kt
@@ -1,15 +1,24 @@
 package com.one.tic_tac_toe1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        val id = findViewById<TextView>(R.id.tv)
-        
+        val splashScreen = findViewById<TextView>(R.id.tv)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+
     }
 }
